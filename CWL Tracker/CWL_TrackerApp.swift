91a80d914eb4185cef983ce391d560aca96d2c87
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-12-29 11:05:30 -0600

    Initial Commit

---
+++ CWL Tracker/CWL_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWL_TrackerApp.swift
+//  CWL Tracker
+//
+//  Created by Jakob McKenna on 2023-12-29.
+//
+
+import SwiftUI
+
+@main
+struct CWL_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
