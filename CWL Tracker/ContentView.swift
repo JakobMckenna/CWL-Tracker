b8478dfdef17e68d4a9120a06166a5ee09965872
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-12-31 12:28:24 -0600

    Fixed button and cleaned up the stacks

--- CWL Tracker/ContentView.swift
+++ CWL Tracker/ContentView.swift
@@ -14,63 +14,56 @@ struct ContentView: View {
     @State private var selection: String? = nil
    
     var body: some View {
-        
-        ZStack {
-            Color(red: 0.9, green: 0.45, blue: 0)
-                .ignoresSafeArea()
-            
-            VStack {
-                Text("CWL Tracker")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color(hue: 0.111, saturation: 1.0, brightness: 1.0))
-                    .multilineTextAlignment(.center)
-                    .padding()
-                Image("Trophy")
+        NavigationView {
+            ZStack {
+                Color(red: 0.9, green: 0.45, blue: 0)
+                    .ignoresSafeArea()
                 
-                Text("Enter your clan tag:")
-                    .font(.title)
-                
-                HStack {
-                    TextField("clan tag", text: $clanTag)
-                        .textFieldStyle(.roundedBorder)
+                VStack {
+                    Text("CWL Tracker")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(hue: 0.111, saturation: 1.0, brightness: 1.0))
+                        .multilineTextAlignment(.center)
                         .padding()
-    
-                }
-                
-                NavigationView {
-                    VStack {
-                        Color(red: 0.9, green: 0.45, blue: 0)
-                            .ignoresSafeArea()
-                        NavigationLink(destination: ContentView2(), tag: "A", selection: $selection) {}
-                            Button(action: {
-                                var clanInput = clanTag
-
-                                //check and/or add # before the clan tag
-                                if (clanInput.contains("#")) {
-                                    //we are good to go
-                                }
-                                else {
-                                    //add # at the start
-                                    clanInput = "#" + clanInput
-                                }
-                                //eventually we will do api call here
-                                print(clanInput)
-                                selection = "A"
-                                 
-                            }){
-                                Text("Link clan")
-                                    .foregroundColor(.black)
-                                    .padding(.vertical, 10)
-                                    .frame(width: 200)
-                            }
-                            .background(Color.orange)
-                            .clipShape(Capsule())
-                        
-                        Color(red: 0.9, green: 0.45, blue: 0)
-                            .ignoresSafeArea()
-                        }
+                    Image("Trophy")
+                    
+                    Text("Enter your clan tag:")
+                        .font(.title)
+                    
+                    HStack {
+                        TextField("clan tag", text: $clanTag)
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+        
                     }
+                    
+                    NavigationLink(destination: ContentView2(), tag: "A", selection: $selection) {}
+                        Button(action: {
+                            var clanInput = clanTag
+
+                            //check and/or add # before the clan tag
+                            if (clanInput.contains("#")) {
+                                //we are good to go
+                            }
+                            else {
+                                //add # at the start
+                                clanInput = "#" + clanInput
+                            }
+                            //eventually we will do api call here
+                            print(clanInput)
+                            selection = "A"
+                             
+                        }){
+                            Text("Link clan")
+                                .foregroundColor(.black)
+                                .padding(.vertical, 10)
+                                .frame(width: 200)
+                        }
+                        .background(Color.orange)
+                        .clipShape(Capsule())
+                        }
+                    
             }
         }
         
